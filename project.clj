ad9425f687acb17b1b4a9857417f311ab20c4ace
Diana Thayer <garbados@gmail.com>
2026-06-17 00:55:55 -0700

    metadata edits

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject becoming-novel "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://example.com/FIXME"
+  :description "Becoming, a novel."
+  :url "https://garbados.github.io/becoming-novel/"
   :main becoming-novel.build
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "CC BY-NC-ND"
+            :url "https://creativecommons.org/licenses/by-nc-nd/4.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
                  [markdown-clj "1.12.8"]
                  [hbs "1.1.0"]]
